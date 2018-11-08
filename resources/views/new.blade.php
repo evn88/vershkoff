d49e2a39eb4bde61692d775b24b52b8edf96e4eb
Vershkov Egor <evn88fx64@gmail.com>
2018-11-08 21:06:31 +0400

    template / add object-fit: cover

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet"> -->
     <title>Фотограф Вершкова Анна</title>
