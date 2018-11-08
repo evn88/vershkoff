f62863d915f2791a1b3b1a040a0fe60adc5e8ff4
Vershkov Egor <evn88fx64@gmail.com>
2018-11-08 22:10:10 +0400

    template /  без закруглений

--- resources/views/new.blade.php
+++ resources/views/new.blade.php
@@ -42,15 +42,15 @@
             <span>Наталья с Варенькой и Зоей</span>
         </a>
         <a href="#">
-            <img src="http://vershkoff.ru/storage/cache/images/000/607/IMG-5553,300.225.85.60.crop.2x.1534768508.JPG" alt="Черногория">    
+            <img src="http://vershkoff.ru/storage/cache/images/000/579/IMG-7573,300.225.85.60.crop.2x.1534757406.JPG">    
             <!-- <span>Название альбома 4</span> -->
         </a>
         <a href="#">
-            <img src="http://vershkoff.ru/storage/cache/images/000/754/IMG-6998v,300.225.85.60.crop.2x.1539160432.jpg" alt="Черногория">   
-            <span>Название альбома 5</span>
+            <img src="http://vershkoff.ru/storage/cache/images/000/490/IMG-9956,300.225.85.60.crop.2x.1534586460.JPG">   
+            <span>Семья Вершковых</span>
         </a>
         <a href="#">
-            <img src="http://vershkoff.ru/storage/cache/images/000/607/IMG-5553,300.225.85.60.crop.2x.1534768508.JPG">    
+            <img src="http://vershkoff.ru/storage/cache/images/000/711/IMG-3832veb,420.315.85.60.crop.2x.1538217843.jpg">    
             <span>Название альбома 6</span>
         </a>
         <a href="#">
